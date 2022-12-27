6425c0e3ca411bd7be2a231ffd11a7f257d9d011
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-12-27 19:27:44 +0600

    added firebase and Authprovider

---
+++ src/AuthProvider/AuthProvider.js
@@ -0,0 +1,95 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from './../Firebase/Firebase.config';
+
+const auth = getAuth(app);
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+ const [user,setUser]  = useState(null);
+ const [loading,setLoading] = useState(false);
+ 
+
+ // create user by email and password 
+ const createUser = (email,password) =>  {
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth,email,password)
+ }
+
+ // create  user by google 
+ const GoogleLogIn = (provider) => {
+    setLoading(true)
+    return signInWithPopup(auth,provider)
+ }
+
+ // loginuser by email and password 
+ const LogInUser = (email,password) => {
+    setLoading(true)
+   return signInWithEmailAndPassword(auth,email,password)
+ }
+
+// LogOut 
+const LogOut = () =>  {
+    setLoading(true)
+    return signOut(auth)
+}
+
+// send email for verification  user 
+const VerifyUser = () => {
+    setLoading(true)
+    return sendEmailVerification(auth.currentUser)
+}
+
+// send password reset email 
+const PasswordReset = (email) => {
+    setLoading(true)
+    return sendPasswordResetEmail(auth,email)
+}
+
+// update user name 
+const UpdateUser = (userInfo) => {
+    setLoading(true)
+    return updateProfile(auth.currentUser, userInfo)
+}
+
+useEffect(()=> {
+    const unsubscribe = onAuthStateChanged(auth,currentUser=> {
+        setUser(currentUser)
+        setLoading(false);
+         // the condition work  properly to verify user and access  to login 
+
+        // if(currentUser.emailVerified){
+        //     setUser(currentUser)
+
+        // }
+        //  if (!currentUser.emailVerified) { 
+          
+        //     setUser({})
+          
+        // }
+        
+    })
+    return ()=> unsubscribe()  ;
+},[])
+
+const userAuth = {
+    user,
+    loading,
+    createUser,
+    GoogleLogIn,
+    LogInUser,
+    LogOut,
+    VerifyUser,
+    PasswordReset,
+    UpdateUser
+}
+
+return (
+    <AuthContext.Provider  value={userAuth} > 
+        {children}
+    </AuthContext.Provider>
+)
+
+};
+
+export default AuthProvider;
